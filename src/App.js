599e2dcac0712c53e641b704231dbe897dfa425f
Андрей Ильин <a.ilin.v@yandex.ru>
2023-05-31 13:11:10 +0300

    upd::auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import Router from "./components/router/Router";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const defaultState = {
+    token: "",
+};
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case "SET_TOKEN":
+            return { ...state, token: action.payload };
+        default:
+            return state;
+    }
+};
+const store = createStore(reducer);
 
 function App() {
-    return <Router />;
+    return (
+        <Provider store={store}>
+            <Router />
+        </Provider>
+    );
 }
 
 export default App;
--- src/components/require-auth/RequireAuth.jsx
+++ src/components/require-auth/RequireAuth.jsx
@@ -1,5 +1,9 @@
-const RequireAuth = ({ children }) => {
-    return children;
+import { useSelector } from "react-redux";
+
+const RequireAuth = ({ children = <div>Not Found</div> }) => {
+    const token = useSelector((state) => state.token);
+    if (token) return children;
+    return <div>Not Found</div>;
 };
 
 export default RequireAuth;
