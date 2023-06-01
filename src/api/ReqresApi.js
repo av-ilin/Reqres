50e1470c698d68740690bea93093084673af985b
Андрей Ильин <a.ilin.v@yandex.ru>
2023-06-01 17:19:39 +0300

    upd::api

--- src/api/ReqresApi.js
+++ src/api/ReqresApi.js
@@ -1,5 +1,11 @@
 class ReqresApi {
     static path = "https://reqres.in/api/";
+    static resource = "unknown";
 
-    async getResource() {}
+    async getResource() {
+        const url = ReqresApi.path + ReqresApi.resource;
+        const response = await fetch(url);
+        const data = response.json();
+        return data;
+    }
 }
